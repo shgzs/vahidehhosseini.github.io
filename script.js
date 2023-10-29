b43c692fb5b30afff9e5538f337eb0cc77466517
Hadi Ghasemi <arapardaz@gmail.com>
2023-10-29 23:43:51 +0330

    2nd version. copy/paste from monica AI

---
+++ script.js
@@ -0,0 +1,18 @@
+// This is an example of a simple script.js file
+
+// When the page loads, display an alert message
+window.onload = function () {
+    alert("Welcome to my website!");
+};
+
+// When the user clicks on a button with the ID "btn-coaching",
+// display a message about your coaching services
+document.getElementById("btn-coaching").addEventListener("click", function () {
+    alert("I offer coaching services to help you achieve your goals!");
+});
+
+// When the user clicks on a button with the ID "btn-contact",
+// display a message about how to contact you
+document.getElementById("btn-contact").addEventListener("click", function () {
+    alert("You can contact me at [your email address].");
+});
